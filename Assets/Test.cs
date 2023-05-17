61366bb9862f6613a46c24aa33ebcc8a51d4adae
tiger-ri <tora237470@gmail.com>
2023-05-17 17:40:10 +0900

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,23 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public class Boss
+{
+    private int hp = 100;          // 体力
+    private int power = 25;        // 攻撃力
+    private int mp = 53;           // MP
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+    //マジック攻撃用の関数
+    public void Magic()
+    {
+        //残りmpを減らす
+        this.mp -= 5;
+        if (mp >= 3)
+        { 
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため、魔法が使えない");
+        }
+        
+    }
+}
+
+
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //配列を初期化する
-        int[] array = { 23, 27, 70, 74, 16 };
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
 
-        //配列の各要素を順番に表示
-        for (int i = 0; i < array.Length; i++)
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+        //マジック攻撃用の関数を呼び出す
+        for (int i = 0; i < 11; i++)
         {
-            Debug.Log (array [i]);
-        }
-        //配列の各要素を逆順に表示
-        for (int i = 4; i >=0; i--)
-        {
-            Debug.Log(array[i]);
+            lastboss.Magic();
         }
     }
 
